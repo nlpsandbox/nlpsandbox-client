b2481e9e3248c9230471e23b149f9f5795495a85
tschaffter <thomas.schaffter@gmail.com>
2020-10-10 16:25:53 -0700

    Experimenting with click.CommandCollection

---
+++ nlpsandboxclient/cli/community.py
@@ -0,0 +1,20 @@
+import click
+
+# Command Group
+@click.group()
+def cli_tools():
+    """Tool related commands"""
+    pass
+
+@cli_tools.command(name='install', help='test install')
+@click.option('--test1', default='1', help='test option')
+def install_cmd(test1):
+    click.echo('Hello world')
+
+@cli_tools.command(name='search', help='test search')
+@click.option('--test1', default='1', help='test option')
+def search_cmd(test1):
+    click.echo('Hello world')
+
+if __name__ == '__main__':
+    cli_tools()
---
+++ nlpsandboxclient/cli/evaluation.py
@@ -0,0 +1,20 @@
+import click
+
+# Command Group
+@click.group()
+def cli_evaluation():
+    """Tool related commands"""
+    pass
+
+@cli_evaluation.command(name='install2', help='test install')
+@click.option('--test1', default='1', help='test option')
+def install_cmd(test1):
+    click.echo('Hello world')
+
+@cli_evaluation.command(name='search2', help='test search')
+@click.option('--test1', default='1', help='test option')
+def search_cmd(test1):
+    click.echo('Hello world')
+
+if __name__ == '__main__':
+    cli_evaluation()
