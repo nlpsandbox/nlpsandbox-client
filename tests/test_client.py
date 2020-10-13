4e108d6c18f43366540fb82c4f05fb7160a30a80
thomasyu888 <thomas.yu@sagebase.org>
2020-10-13 15:39:10 -0700

    Add tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -12,15 +12,25 @@ class TestClient:
     def setup_method(self):
         """Method called once per method"""
         print("foo")
-        self.Nlp = NlpClient(data_node_endpoint=DATA_NODE_ENDPOINT)
+        self.nlp = NlpClient(data_node_endpoint=DATA_NODE_ENDPOINT)
 
     def test_get_clinical_notes(self):
-        with patch.object(self.Nlp, "rest_get") as rest_get:
-            self.Nlp.get_clinical_notes()
+        with patch.object(self.nlp, "rest_get") as rest_get:
+            self.nlp.get_clinical_notes()
             rest_get.assert_called_once_with("/notes")
 
     def test_get_clinical_note(self):
-        with patch.object(self.Nlp, "rest_get") as rest_get:
-            self.Nlp.get_clinical_note("12345")
+        with patch.object(self.nlp, "rest_get") as rest_get:
+            self.nlp.get_clinical_note("12345")
             rest_get.assert_called_once_with("/notes/12345")
+
+    def test_get_health(self):
+        with patch.object(self.nlp, "rest_get") as rest_get:
+            self.nlp.get_health()
+            rest_get.assert_called_once_with("/health")
+
+    def test_get_dates(self):
+        with patch.object(self.nlp, "rest_get") as rest_get:
+            self.nlp.get_dates()
+            rest_get.assert_called_once_with("/annotations/dates")
     
