42013d793ae3f66124c6801b827f148b1d664ef2
thomasyu888 <thomas.yu@sagebase.org>
2020-10-14 15:30:33 -0700

    Get clinical notes cli

--- nlpsandboxclient/cli/community.py
+++ nlpsandboxclient/cli/community.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+import json
 
 import click
 import synapseclient
+
+from nlpsandboxclient import client
+from nlpsandboxclient.client import NlpClient
 
 
 # Command Group
@@ -16,6 +20,20 @@ def get_num_users():
     syn = synapseclient.login()
     res = syn.restGET("/teamMembers/count/3413388")
     print(res)
+
+
+@cli.command()
+@click.option('--output', help='Output filepath', type=click.Path())
+def get_clinical_notes(output):
+    """Gets all the clinical notes"""
+    Nlp = NlpClient(data_node_endpoint=client.DATA_NODE_ENDPOINT)
+    clinical_notes = Nlp.get_clinical_notes()
+    # Stdout or store to json
+    if output is None:
+        print(clinical_notes)
+    else:
+        with open(output, 'w') as json_o:
+            json.dump(clinical_notes, json_o)
 
 
 if __name__ == '__main__':
