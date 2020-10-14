d84bfe6faad008d1b3d651d19b99bc6dbee44b1f
thomasyu888 <thomas.yu@sagebase.org>
2020-10-14 15:46:37 -0700

    Fix

--- nlpsandboxclient/cli/community.py
+++ nlpsandboxclient/cli/community.py
@@ -33,7 +33,7 @@ def get_clinical_notes(output):
 
 
 @cli.command()
-@click.argument('noteid', help='Clinical note ID', type=int)
+@click.argument('noteid', help='Clinical note ID', type=click.INT)
 @click.option('--output', help='Output filepath', type=click.Path())
 def get_clinical_note(noteid, output):
     """Gets all the clinical notes"""
