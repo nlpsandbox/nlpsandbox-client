91c12c485035b6b37f384644b680405db61fd25e
thomasyu888 <thomas.yu@sagebase.org>
2020-10-27 10:59:45 -0700

    Comment out validation

--- nlpsandboxclient/validation.py
+++ nlpsandboxclient/validation.py
@@ -11,25 +11,34 @@ class NlpToolClient(NlpClient):
         super().__init__(*args, **kwargs)
         self.tool_type = tool_type
 
+    def get_tool(self):
+        """Get tool schema"""
+        return self.rest_get("/tool")
+
     def validate(self):
         """Validate schema"""
         # Check schema is correct first
+        if self.get_ui().status_code != 200:
+            raise ValueError("/ui endpoint not configured")
+        if self.get_health().get('status') != 'pass':
+            raise ValueError("/health endpoint not configured")
+        schema = self.get_tool()
         Draft7Validator.check_schema(schema)
         schema_validator = Draft7Validator(schema)
         # Create error message to location mapping
-        error_map = {}
-        for error in schema_validator.iter_errors(data):
-            if error_map.get(error.message) is None:
-                error_map[error.message] = [_parse_path(error.absolute_path)]
-            else:
-                error_map[error.message].append(_parse_path(error.absolute_path))
-        final_error_list = []
-        for error in error_map:
-            # If the json is completely incorrect, it will have error list ['']
-            if error_map[error] == ['']:
-                error_string = f'{error}\n'
-            else:
-                errors = "\n  ".join(error_map[error])
-                error_string = f'{error} at\n  {errors}\n'
-            final_error_list.append(error_string)
-        return final_error_list
+        # error_map = {}
+        # for error in schema_validator.iter_errors(data):
+        #     if error_map.get(error.message) is None:
+        #         error_map[error.message] = [_parse_path(error.absolute_path)]
+        #     else:
+        #         error_map[error.message].append(_parse_path(error.absolute_path))
+        # final_error_list = []
+        # for error in error_map:
+        #     # If the json is completely incorrect, it will have error list ['']
+        #     if error_map[error] == ['']:
+        #         error_string = f'{error}\n'
+        #     else:
+        #         errors = "\n  ".join(error_map[error])
+        #         error_string = f'{error} at\n  {errors}\n'
+        #     final_error_list.append(error_string)
+        # return final_error_list
