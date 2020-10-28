6c4e608a088cc7c7b3700f457a9f65c6927b7489
thomasyu888 <thomas.yu@sagebase.org>
2020-10-27 17:03:56 -0700

    Validate

--- nlpsandboxclient/validation.py
+++ nlpsandboxclient/validation.py
@@ -1,6 +1,6 @@
 """Validation of data and platform components like NLP Tools"""
 
-from jsonschema import Draft7Validator
+from jsonschema import validate
 
 from .client import NlpClient
 
@@ -22,23 +22,14 @@ class NlpToolClient(NlpClient):
             raise ValueError("/ui endpoint not configured")
         if self.get_health().get('status') != 'pass':
             raise ValueError("/health endpoint not configured")
-        schema = self.get_tool()
-        Draft7Validator.check_schema(schema)
-        schema_validator = Draft7Validator(schema)
-        # Create error message to location mapping
-        # error_map = {}
-        # for error in schema_validator.iter_errors(data):
-        #     if error_map.get(error.message) is None:
-        #         error_map[error.message] = [_parse_path(error.absolute_path)]
-        #     else:
-        #         error_map[error.message].append(_parse_path(error.absolute_path))
-        # final_error_list = []
-        # for error in error_map:
-        #     # If the json is completely incorrect, it will have error list ['']
-        #     if error_map[error] == ['']:
-        #         error_string = f'{error}\n'
-        #     else:
-        #         errors = "\n  ".join(error_map[error])
-        #         error_string = f'{error} at\n  {errors}\n'
-        #     final_error_list.append(error_string)
-        # return final_error_list
+        # Fake schema
+        schema = {
+            "type" : "object",
+            "properties" : {
+                "price" : {"type" : "number"},
+                "name" : {"type" : "string"},
+            },
+        }
+        data = self.get_tool()
+        # If no exception is raised by validate(), the instance is valid.
+        validate(instance=data, schema=schema)
