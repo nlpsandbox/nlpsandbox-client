fd7c910444e89194454d027fe9b7a145a6c9e266
thomasyu888 <thomas.yu@sagebase.org>
2020-11-21 08:21:41 +0800

    Fix

--- nlpsandboxclient/client.py
+++ nlpsandboxclient/client.py
@@ -6,15 +6,15 @@ def get_dataset_clinical_notes(host, datasetid):
     """Get all clinical notes for a dataset"""
     # host = "http://10.23.55.45:8080/api/v1"
     nlp = DataNodeClient(host=host)
-    fhir_stores = nlp.get_fhir_stores(datasetid=datasetid)
+    fhir_stores = nlp.list_fhir_stores(datasetid=datasetid)
     # Obtain fhir store ids
     fhir_store_ids = [fhir_store['name'].split("/")[3]
                       for fhir_store in fhir_stores['fhirStores']]
     all_notes = []
     # Obtain all clinical notes for all fhir stores in a dataset
     for fhir_store_id in fhir_store_ids:
-        clinical_notes = nlp.get_clinical_notes(datasetid=datasetid,
-                                                storeid=fhir_store_id)
+        clinical_notes = nlp.list_clinical_notes(datasetid=datasetid,
+                                                 storeid=fhir_store_id)
         # Obtain all clinical notes
         for note in clinical_notes:
             all_notes.extend(note['notes'])
