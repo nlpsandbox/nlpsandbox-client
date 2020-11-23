7630407beea4fbcf34743cce010594a63d9e6917
thomasyu888 <thomas.yu@sagebase.org>
2020-11-23 10:15:40 +0800

    Update client to use name

--- nlpsandboxclient/client.py
+++ nlpsandboxclient/client.py
@@ -1,64 +1,91 @@
 """NLP client object"""
 import os
+import re
 
 from .api_client import DataNodeApiClient
 
 
-def extract_name(responses, key):
+def extract_name(responses: list, key: str):
     """Extract the name annotations from rest call responses
 
     Args:
+        responses: list of response bodies
         key: Key that stores the returned names
     """
     for response_info in responses:
         for response in response_info[key]:
-            yield os.path.basename(response['name'])
+            yield response['name']
+
+
+def get_inputs_from_name(name: str, regex: str):
+    """Get API input parameters from API output
+
+    Args:
+        name: Name of resource
+        regex: String matching rule
+
+    Returns:
+        matched groups
+    """
+    match = re.match(regex, name)
+    if not match:
+        raise ValueError("Invalid name")
+    return match
 
 
 class DataNodeClient:
     """Data node client"""
-    def __init__(self, host=None):
+    def __init__(self, host: str = None):
         self.client = DataNodeApiClient(host=host)
 
     def list_datasets(self):
         """Lists dataset ids"""
         datasets = self.client.list_datasets()
         return extract_name(datasets, "datasets")
-        # for dataset_info in datasets:
-        #     for dataset in dataset_info['datasets']:
-        #         yield dataset['name'].replace("datasets/", "")
 
-    def create_dataset(self, datasetid=None):
+    def create_dataset(self, datasetname: str):
         """Create a dataset"""
-        return self.client.create_dataset(datasetid=datasetid)
+        match = get_inputs_from_name(datasetname, "datasets/(.*)")
+        return self.client.create_dataset(datasetid=match.group(1))
 
-    def list_annotation_stores(self, datasetid=None):
+    def list_annotation_stores(self, datasetname):
         """List the annotation stores for a dataset"""
+        match = get_inputs_from_name(datasetname, "datasets/(.*)")
         annotation_stores = self.client.list_annotation_stores(
-            datasetid=datasetid
+            datasetid=match.group(1)
         )
         return extract_name(annotation_stores, "annotationStores")
 
-#     def create_annotation_store(self, datasetid=None, storeid=None):
-#         """Create an annotation store"""
-#         return self.rest_post(
-#             f"/datasets/{datasetid}/annotationStore?"
-#             f"annotationStoreId={storeid}",
-#             body=json.dumps({})
-#         )
+    def create_annotation_store(self, datasetname=None,
+                                annotation_storeid=None):
+        """Create an annotation store"""
+        match = get_inputs_from_name(datasetname, "datasets/(.*)")
+        return self.client.create_annotation_store(datasetid=match.group(1),
+                                                   storeid=annotation_storeid)
 
-#     def list_annotations(self, datasetid=None, storeid=None):
-#         """List the annotations for an annotation store"""
-#         return self.rest_get_paginated(
-#             f"/datasets/{datasetid}/annotationStore/{storeid}/annotations"
-#         )
+    def list_annotations(self, annotation_store_name=None):
+        """List the annotations for an annotation store"""
+        match = get_inputs_from_name(
+            annotation_store_name,
+            "datasets/(.*)/annotationStore/(.*)$"
+        )
+        annotations = self.client.list_annotations(
+            datasetid=match.group(1), storeid=match.group(2)
+        )
+        for annotation in annotations:
+            for annot in annotation['annotations']:
+                yield annot
 
-#     def get_annotation(self, datasetid=None, storeid=None, annotationid=None):
-#         """Get an annotation"""
-#         return self.rest_get(
-#             f"/datasets/{datasetid}/annotationStore/{storeid}/"
-#             f"annotations/{annotationid}"
-#         )
+    def get_annotation(self, annotation_name):
+        """Get an annotation"""
+        match = get_inputs_from_name(
+            annotation_name,
+            "datasets/(.*)/annotationStore/(.*)/annotations/(.*)$"
+        )
+        return self.client.get_annotation(
+            datasetid=match.group(1), storeid=match.group(2),
+            annotationid=match.group(3)
+        )
 
 #     def create_annotation(self, datasetid, storeid, annotation):
 #         """Create an annotation"""
@@ -124,7 +151,7 @@ class DataNodeClient:
 def get_dataset_clinical_notes(host, datasetid):
     """Get all clinical notes for a dataset"""
     # host = "http://10.23.55.45:8080/api/v1"
-    nlp = DataNodeClient(host=host)
+    nlp = DataNodeApiClient(host=host)
     fhir_stores = nlp.list_fhir_stores(datasetid=datasetid)
     # Obtain fhir store ids
     fhir_store_ids = [fhir_store['name'].split("/")[3]
