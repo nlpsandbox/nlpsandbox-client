fa2b06e4465fe4c04c376ddf04ce57b331bc0a2d
thomasyu888 <thomas.yu@sagebase.org>
2020-11-25 08:59:24 +0800

    Lint

--- nlpsandboxclient/client.py
+++ nlpsandboxclient/client.py
@@ -1,14 +1,11 @@
 """NLP client object"""
-import os
-import re
-
 from . import api_client
 from .api_client import DataNodeApiClient
 from . import utils
 
 
 class DataNodeClient:
-    """Data node client"""
+    """Data node client that utilizes the API client"""
     def __init__(self, host: str = None):
         self.client = DataNodeApiClient(host=host)
 
@@ -22,7 +19,7 @@ class DataNodeClient:
         match = utils.get_inputs_from_name(datasetname, "datasets/(.*)")
         return self.client.create_dataset(datasetid=match.group(1))
 
-    def list_annotation_stores(self, datasetname):
+    def list_annotation_stores(self, datasetname: str):
         """List the annotation stores for a dataset"""
         match = utils.get_inputs_from_name(datasetname, "datasets/(.*)")
         annotation_stores = self.client.list_annotation_stores(
@@ -30,15 +27,14 @@ class DataNodeClient:
         )
         return utils.extract_name(annotation_stores, "annotationStores")
 
-    def create_annotation_store(self, datasetname=None,
-                                annotation_storeid=None):
+    def create_annotation_store(self, datasetname: str, annotation_storeid: str):
         """Create an annotation store"""
         match = utils.get_inputs_from_name(datasetname, "datasets/(.*)")
         return self.client.create_annotation_store(
             datasetid=match.group(1), annotation_storeid=annotation_storeid
         )
 
-    def list_annotations(self, annotation_store_name=None):
+    def list_annotations(self, annotation_store_name: str):
         """List the annotations for an annotation store"""
         match = utils.get_inputs_from_name(
             annotation_store_name,
@@ -51,7 +47,7 @@ class DataNodeClient:
             for annot in annotation['annotations']:
                 yield annot
 
-    def get_annotation(self, annotation_name):
+    def get_annotation(self, annotation_name: str):
         """Get an annotation"""
         match = utils.get_inputs_from_name(
             annotation_name,
@@ -62,7 +58,7 @@ class DataNodeClient:
             annotationid=match.group(3)
         )
 
-    def create_annotation(self, annotation_store_name, annotation):
+    def create_annotation(self, annotation_store_name: str, annotation: str):
         """Create an annotation"""
         match = utils.get_inputs_from_name(
             annotation_store_name,
@@ -74,19 +70,19 @@ class DataNodeClient:
             annotation=annotation
         )
 
-    def list_fhir_stores(self, datasetname):
+    def list_fhir_stores(self, datasetname: str):
         """List the FHIR stores in a dataset"""
         match = utils.get_inputs_from_name(datasetname, "datasets/(.*)")
         fhir_stores=  self.client.list_fhir_stores(datasetid=match.group(1))
         return utils.extract_name(fhir_stores, "fhirStores")
 
-    def create_fhir_store(self, datasetname, fhirstoreid):
+    def create_fhir_store(self, datasetname: str, fhirstoreid: str):
         """Create a FHIR store"""
         match = utils.get_inputs_from_name(datasetname, "datasets/(.*)")
         return self.client.create_fhir_store(datasetid=match.group(1),
                                              fhir_storeid=fhirstoreid)
 
-    def list_clinical_notes(self, fhirstore_name):
+    def list_clinical_notes(self, fhirstore_name: str):
         """List clinical notes in a FHIR store"""
         match = utils.get_inputs_from_name(fhirstore_name,
                                            "datasets/(.*)/fhirStores/(.*)$")
@@ -95,7 +91,7 @@ class DataNodeClient:
         for note in notes:
             yield note['notes']
 
-    def get_clinical_note(self, fhirstore_name, noteid):
+    def get_clinical_note(self, fhirstore_name: str, noteid: str):
         """Get a clinical note"""
         match = utils.get_inputs_from_name(fhirstore_name,
                                            "datasets/(.*)/fhirStores/(.*)$")
@@ -103,7 +99,7 @@ class DataNodeClient:
                                              fhir_storeid=match.group(2),
                                              noteid=noteid)
 
-    def create_clinical_note(self, fhirstore_name, noteid):
+    def create_clinical_note(self, fhirstore_name: str, noteid: str):
         """Create a clinical note"""
         match = utils.get_inputs_from_name(fhirstore_name,
                                            "datasets/(.*)/fhirStores/(.*)$")
@@ -111,7 +107,7 @@ class DataNodeClient:
                                                 fhir_storeid=match.group(2),
                                                 note=noteid)
 
-    def list_patients(self, fhirstore_name):
+    def list_patients(self, fhirstore_name: str):
         """Lists the patients in a FHIR store"""
         match = utils.get_inputs_from_name(fhirstore_name,
                                            "datasets/(.*)/fhirStores/(.*)$")
@@ -120,7 +116,7 @@ class DataNodeClient:
         for patient in patients:
             yield patient['patients']
 
-    def get_patient(self, fhirstore_name, patientid):
+    def get_patient(self, fhirstore_name: str, patientid: str):
         """Get a FHIR patient"""
         match = utils.get_inputs_from_name(fhirstore_name,
                                            "datasets/(.*)/fhirStores/(.*)$")
@@ -128,7 +124,7 @@ class DataNodeClient:
                                        fhir_storeid=match.group(2),
                                        patientid=patientid)
 
-    def create_patient(self, fhirstore_name, patientid):
+    def create_patient(self, fhirstore_name: str, patientid: str):
         """Create a FHIR patient"""
         match = utils.get_inputs_from_name(fhirstore_name,
                                            "datasets/(.*)/fhirStores/(.*)$")
