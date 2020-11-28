4b031916256d3e49fe08cbee93503f9108f90863
thomasyu888 <thomas.yu@sagebase.org>
2020-11-29 07:48:20 +0800

    Change ...id to ..._id

--- nlpsandboxclient/client.py
+++ nlpsandboxclient/client.py
@@ -4,16 +4,16 @@ from typing import List
 from .api_client import DataNodeApiClient
 
 
-def get_clinical_notes(host: str, datasetid: str) -> List[dict]:
+def get_clinical_notes(host: str, dataset_id: str) -> List[dict]:
     """Get all clinical notes for a dataset"""
     nlp = DataNodeApiClient(host=host)
-    fhir_stores = nlp.list_fhir_stores(datasetid=datasetid)
+    fhir_stores = nlp.list_fhir_stores(dataset_id=dataset_id)
 
     all_notes = []
     # Obtain all clinical notes for all fhir stores in a dataset
     for fhir_store in fhir_stores:
         clinical_notes = nlp.list_clinical_notes(
-            datasetid=datasetid, fhir_storeid=fhir_store.id
+            dataset_id=dataset_id, fhir_store_id=fhir_store.id
         )
         # Obtain all clinical notes
         for note in clinical_notes:
@@ -22,6 +22,6 @@ def get_clinical_notes(host: str, datasetid: str) -> List[dict]:
                 "noteType": note.note_type,
                 "patientId": note.patientid,
                 "text": note.text,
-                "note_name": f"dataset/{datasetid}/fhirStores/{fhir_store.id}/fhir/Note/{note.id}"
+                "note_name": f"dataset/{dataset_id}/fhirStores/{fhir_store.id}/fhir/Note/{note.id}"
             })
     return all_notes
