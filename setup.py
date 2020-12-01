3a7f3796d14b1597744fb7f9dc703bb75792ab92
tschaffter <thomas.schaffter@gmail.com>
2020-12-01 07:20:37 -0800

    Update CI with new Docker publish job

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     # basic
     name='nlpsandbox-client',
-    version='0.1.1',
+    version='0.2.0',
     # packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
     # py_modules=['hello'],
     # scripts=['bin/nlp-evaluate'],
