f06725813e5adf23897e83d47510558bb4f895d0
thomasyu888 <thomas.yu@sagebase.org>
2020-12-04 10:21:20 +0800

    Remove other annotator

--- personannotator/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from personannotator.api.service_api import ServiceApi
-from personannotator.api.text_person_name_annotation_api import TextPersonNameAnnotationApi
