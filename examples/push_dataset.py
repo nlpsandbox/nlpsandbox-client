ad59ef08fea232f0ded605f4dfe7d6f9950246be
Thomas Yu <thomas.yu@sagebase.org>
2021-03-16 06:06:27 +0800

    Update push_dataset.py to delete dataset first (#136)
    * Update example
    
    * Remove comment
    
    * Add to docstring
    
    * Fix
    
    Co-authored-by: Thomas Schaffter <thomas.schaffter@gmail.com>

--- examples/push_dataset.py
+++ examples/push_dataset.py
@@ -1,6 +1,15 @@
 """
 Example code to push a dataset into the data node. A complete
-dataset includes
+dataset includes "Dataset", "Fhir Store", "Annotation Store",
+"Annotation", "Patient", "Note"
+
+To run this code, here are the requirements:
+
+- Install the nlpsandbox-client (`pip install nlpsandbox-client`)
+- Start the Data Node locally - Follow instructions here:
+https://github.com/nlpsandbox/data-node
+- python push_dataset.py
+
 """
 import json
 
@@ -11,45 +20,10 @@ from datanode.rest import ApiException
 import nlpsandboxclient.utils
 
 
-def get_or_create_resource(get_func, create_func, *args, **kwargs):
-    """Get or create a data node resource
-
-    Args:
-        get_func: Function to get resource
-        create_func: Function to create resource
-        *args: Positional arguments for the get/create functions
-        **kwargs: Keyword arguments for the get/create functions
-
-    Returns:
-        resource object
-
-    """
-    try:
-        # get the dataset
-        resource = get_func(*args)
-    except ApiException as e:
-        if e.status == 404:
-            # create dataset if not found
-            try:
-                resource = create_func(
-                    *args,
-                    **kwargs
-                )
-            except ApiException as e:
-                print(f"Exception when calling {create_func}: {e}\n")
-                raise
-        else:
-            print(f"Exception when calling {get_func}: {e}\n")
-            raise
-    return resource
-
-
 # Defining the host is optional and defaults to http://example.com/api/v1
 # See configuration.py for a list of all supported configuration parameters.
 host = "http://localhost:8080/api/v1"
-configuration = datanode.Configuration(
-    host=host
-)
+configuration = datanode.Configuration(host=host)
 
 dataset_id = 'test-dataset'
 fhir_store_id = 'evaluation'
@@ -65,51 +39,38 @@ with datanode.ApiClient(configuration) as api_client:
     note_api = datanode.apis.NoteApi(api_client)
     annotation_api = datanode.apis.AnnotationApi(api_client)
 
-    # an example of how to create or get a dataset
-    # This logic is abstracted in the `get_or_create_resource`
-    # function above.
+    # The example is always deleted
     try:
         # get the dataset
         dataset = dataset_api.get_dataset(dataset_id)
-    except ApiException as e:
-        if e.status == 404:
-            # create dataset if not found
-            try:
-                dataset = dataset_api.create_dataset(
-                    dataset_id,
-                    body={}
-                )
-            except ApiException as e:
-                print("Exception when calling DatasetApi->create_dataset: %s\n" % e)
-                raise
-        else:
-            print("Exception when calling DatasetApi->get_dataset: %s\n" % e)
-            raise
-    # Get or create FHIR store
-    fhir_store = get_or_create_resource(
-        fhir_store_api.get_fhir_store,
-        fhir_store_api.create_fhir_store,
+        # delete the dataset
+        print(f"Deleting exist dataset: {dataset_id}")
+        dataset_api.delete_dataset(dataset_id)
+    except ApiException:
+        pass
+
+    # create dataset if not found
+    print(f"Creating dataset: {dataset_id}")
+    dataset = dataset_api.create_dataset(
         dataset_id,
-        fhir_store_id,
         body={}
     )
 
-    annotation_store = get_or_create_resource(
-        annotation_store_api.get_annotation_store,
-        annotation_store_api.create_annotation_store,
-        dataset_id,
-        annotation_store_id,
+    print(f"Creating Fhir Store: {fhir_store_id}")
+    fhir_store = fhir_store_api.create_fhir_store(
+        dataset_id, fhir_store_id,
         body={}
     )
 
-    print(f"dataset: {dataset}")
-    print(f"fhir_store: {fhir_store}")
-    print(f"annotation_store: {annotation_store}")
+    print(f"Creating Annotation Store: {annotation_store_id}")
+    annotation_store = annotation_store_api.create_annotation_store(
+        dataset_id, annotation_store_id,
+        body={}
+    )
 
     with open(json_filename) as f:
         data = json.load(f)
         patient_bundles = data['patient_bundles']
-        # patient_bundles = patient_bundles[:1]
 
     for patient_bundle in patient_bundles:
         # Create or get a FHIR Patient
@@ -118,19 +79,14 @@ with datanode.ApiClient(configuration) as api_client:
             nlpsandboxclient.utils.camelcase_to_snakecase
         )
         patient_id = patient.pop("identifier")
-        patient = get_or_create_resource(
-            patient_api.get_patient,
-            patient_api.create_patient,
-            dataset_id,
-            fhir_store_id,
-            patient_id,
+        print(f"Creating patient: {patient_id}")
+        patient_api.create_patient(
+            dataset_id, fhir_store_id, patient_id,
             patient_create_request=patient
         )
-        print(f"patient: {patient}")
 
         # Create the Note and Annotation objects linked to the patient
         note_bundles = patient_bundle['note_bundles']
-        # note_bundles = note_bundles[:1]
 
         for note_bundle in note_bundles:
             # Determine note Id since noteId isn't part of the 'note'
@@ -151,12 +107,10 @@ with datanode.ApiClient(configuration) as api_client:
                 nlpsandboxclient.utils.camelcase_to_snakecase
             )
             note['patient_id'] = patient_id
-            note = get_or_create_resource(
-                note_api.get_note,
-                note_api.create_note,
-                dataset_id,
-                fhir_store_id,
-                note_id,
+
+            print(f"Creating note ({note_id}) for patient ({patient_id})")
+            note_api.create_note(
+                dataset_id, fhir_store_id, note_id,
                 note_create_request=note
             )
             # Create annotation
@@ -169,11 +123,9 @@ with datanode.ApiClient(configuration) as api_client:
                 annotation,
                 nlpsandboxclient.utils.camelcase_to_snakecase
             )
-            annotation = get_or_create_resource(
-                annotation_api.get_annotation,
-                annotation_api.create_annotation,
-                dataset_id,
-                annotation_store_id,
-                note_id,
+            print(f"Creating annotation for note: {note_id}")
+            annotation = annotation_api.create_annotation(
+                dataset_id, annotation_store_id,
+                annotation_id=note_id,
                 annotation_create_request=new_annotation
             )
