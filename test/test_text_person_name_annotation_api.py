36f27aa17c0c1002a0a82f160ac4e87380cf3f23
thomasyu888 <thomas.yu@sagebase.org>
2021-03-20 20:09:54 +0800

    Add test

--- test/test_text_person_name_annotation_api.py
+++ test/test_text_person_name_annotation_api.py
@@ -10,6 +10,7 @@
 
 
 import unittest
+from unittest.mock import patch
 
 import annotator
 from annotator.api.text_person_name_annotation_api import TextPersonNameAnnotationApi  # noqa: E501
@@ -20,16 +21,27 @@ class TestTextPersonNameAnnotationApi(unittest.TestCase):
 
     def setUp(self):
         self.api = TextPersonNameAnnotationApi()  # noqa: E501
+        self.patcher = patch('annotator.api_client.ApiClient.call_api')
+        self.mock_foo = self.patcher.start()
 
     def tearDown(self):
-        pass
+        self.patcher.stop()
 
     def test_create_text_person_name_annotations(self):
         """Test case for create_text_person_name_annotations
 
         Annotate person names in a clinical note  # noqa: E501
         """
-        pass
+        self.api.create_text_person_name_annotations(
+            text_person_name_annotation_request={
+                "note": {
+                    "identifier": "note-1",
+                    "type": "note-type",
+                    "text": "my text here",
+                    "patient_id": "patient-1"
+                }
+            }
+        )
 
 
 if __name__ == '__main__':
