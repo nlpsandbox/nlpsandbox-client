cd4f7dee29692ae988a23440e2703480dbd1698a
jakevc <jake.vancampen7@gmail.com>
2022-12-05 13:12:24 -0800

    fix example calls

--- examples/example_calls.sh
+++ examples/example_calls.sh
@@ -44,14 +44,14 @@ nlp-cli datanode get-annotation --annotation_id name \
                                  --data_node_host http://localhost:8080/api/v1
 
 # Start date-annotator-example
-nlp-cli evaluate get-tool --annotator_host http://localhost:80/api/v1
+nlp-cli tool get-tool --annotator_host http://localhost:80/api/v1
 
-nlp-cli evaluate check-url --url http://localhost:80/api/v1/ui
+nlp-cli tool check-url --url http://localhost:80/api/v1/ui
 
-nlp-cli evaluate annotate-note --annotator_host http://localhost:80/api/v1 \
+nlp-cli tool annotate-note --annotator_host http://localhost:80/api/v1 \
                                --note_json example_note.json \
                                --annotator_type date
 
-nlp-cli evaluate prediction --pred_filepath test/data/new_prediction.json \
+nlp-cli evaluate-prediction --pred_filepath test/data/new_prediction.json \
                             --gold_filepath test/data/new_goldstandard.json \
                             --eval_type person
